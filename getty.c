59099ca86261dd0b1a6ba0432956dfaa1e60d0f4
Daniel Palmer <daniel@thingy.jp>
2026-04-29 16:41:17 +0900

    getty: A bit more filling

--- getty.c
+++ getty.c
@@ -5,14 +5,38 @@
 
 int main(int argc, char **argv, char **envp)
 {
-	const char *tty;
+	const char *shell_path;
+	const char *tty_path;
 
 	if (argc != 3)
 		return 1;
 
-	tty = argv[1];
+	tty_path = argv[1];
+	shell_path = argv[2];
 
-	debug("Starting getty on %s\n", tty);
+	debug("Starting getty on %s with shell %s\n",
+		tty_path, shell_path);
+
+        while (true) {
+		pid_t pid = vfork();
+
+                /* We are getty */
+		if (pid) {
+			wait(NULL);
+		}
+		/* We are the new process */
+		else {
+                        char * const newargv[] = {
+                                "sh",
+                                NULL
+                        };
+                        char *newenviron[] = { NULL };
+
+                        execve(shell_path, newargv, newenviron);
+                        printf("execve failed\n");
+                        break;
+                }
+        }
 
 	while (1) { }
 
