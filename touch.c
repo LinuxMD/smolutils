86f223acfef7e68d9d99cfa0125f7a51a93754d1
Daniel Palmer <daniel@thingy.jp>
2026-05-02 00:36:29 +0900

    touch: Actually do something

--- touch.c
+++ touch.c
@@ -5,5 +5,21 @@
 
 int main (int argc, char **argv, char **envp)
 {
+	const char *path;
+	int __cleanup_fd fd = -1;
+
+	if (argc != 2)
+		return 1;
+
+	path = argv[1];
+
+	/* File doesn't exist, try to create it */
+	if (access(path, F_OK))
+		fd = creat(path, 0644);
+	/* File exists, update timestamp(s) */
+	else {
+		// TODO
+	}
+
 	return 0;
 }
