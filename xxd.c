cd4aa657eb71b8c490ba80c5b7ba04c7ff023d22
Daniel Palmer <daniel@thingy.jp>
2026-05-01 21:54:16 +0900

    xxd: Filling

--- xxd.c
+++ xxd.c
@@ -20,18 +20,36 @@ int main (int argc, char **argv, char **envp)
 
 	for (i = 0; i < sz; i += 0x10) {
 		uint8_t buf[0x10];
+		int ret;
 
-		read(fd, buf, 0x10);
+		/* Read a row's worth of data */
+		ret = read(fd, buf, 0x10);
+		if (ret < 0)
+			return 1;
 
 		printf("%08x: ", i);
+
 		for (j = 0; j < 0x10; j += 2) {
-			printf("%02x%02x ", buf[j], buf[j + 1]);
+			uint8_t lsb = buf[j];
+			uint8_t msb = buf[j + 1];
+			int remaining = ret - j;
+
+			if (remaining <= 0)
+				printf("     ");
+			else if (remaining == 0)
+				printf("%02x   ", lsb);
+			else
+				printf("%02x%02x ", lsb, msb);
 		}
 
 		for (j = 0; j < 0x10; j++) {
 			char ch = buf[j];
+			int remaining = ret - j;
 
-			printf("%c", isprint(ch) ? ch : '.');
+			if (remaining > 0)
+				printf("%c", isprint(ch) ? ch : '.');
+			else
+				printf(" ");
 		}
 
 		printf("\n");
