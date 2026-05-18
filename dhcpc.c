eb0c19b02244e5bbf30ffeedb7b5ff1caab11a65
Daniel Palmer <daniel@thingy.jp>
2026-05-18 20:17:08 +0900

    dhcp: A bit more pudding

--- dhcpc.c
+++ dhcpc.c
@@ -84,25 +84,45 @@ static int build_discover(struct dhcp_packet *p,
 	return offsetof(struct dhcp_packet, options) + opt_count;
 }
 
-static int send_discover(struct context *cntx)
+static int send_discover(struct context *cntx, struct dhcp_packet *p)
 {
 	struct sockaddr_in dst = {
 		.sin_family = AF_INET,
 		.sin_port = htons(SERVER_PORT),
 		.sin_addr.s_addr = INADDR_BROADCAST,
 	};
-	struct dhcp_packet p;
 	int len;
 	int ret;
 
-	len = build_discover(&p, cntx->xid, cntx->mac);
+	len = build_discover(p, cntx->xid, cntx->mac);
 
-	ret = sendto(cntx->sock, &p, len, 0, (struct sockaddr *)&dst, sizeof(dst));
+	ret = sendto(cntx->sock, p, len, 0, (struct sockaddr *)&dst, sizeof(dst));
 
-	if (ret) {
+	if (ret != len) {
 		error("Failed to send discover: %d\n", errno);
 		return -1;
 	}
+
+	return 0;
+}
+
+static int wait_for_packet(struct context *cntx, struct dhcp_packet *p)
+{
+	struct sockaddr_in addr = {
+		.sin_family = AF_INET,
+		.sin_port = htons(SERVER_PORT),
+		.sin_addr.s_addr = INADDR_BROADCAST,
+	};
+	socklen_t addr_len = sizeof(addr);
+	ssize_t recvd;
+
+	recvd  = recvfrom(cntx->sock, p, sizeof(*p), 0, (struct sockaddr *)&addr, &addr_len);
+	if (recvd < 0) {
+		error("Timed out waiting for packet\n");
+		return 1;
+	}
+
+	debug("got packet\n");
 
 	return 0;
 }
@@ -166,9 +186,12 @@ static int setup_socket(struct context *cntx)
 struct ifreq {
 	#define IFHWADDRLEN 6
 	#define	IFNAMSIZ 16
+	#define IFF_UP (1 << 0)
+	#define IFF_RUNNING (1 << 6)
 	#define ifr_name ifr_ifrn.ifrn_name
 	#define ifr_hwaddr ifr_ifru.ifru_hwaddr
-	union
+	#define	ifr_flags ifr_ifru.ifru_flags
+union
 	{
 		char ifrn_name[IFNAMSIZ];
 	} ifr_ifrn;
@@ -179,7 +202,7 @@ struct ifreq {
 //		struct sockaddr ifru_broadaddr;
 //		struct sockaddr ifru_netmask;
 		struct sockaddr ifru_hwaddr;
-//		short ifru_flags;
+		short ifru_flags;
 //		int ifru_ivalue;
 //		int ifru_mtu;
 //		struct ifmap ifru_map;
@@ -189,6 +212,31 @@ struct ifreq {
 //		struct if_settings ifru_settings;
 	} ifr_ifru;
 };
+
+static int ifup(const char *iface)
+{
+	int __cleanup_fd sock = -1;
+	struct ifreq ifr = { 0 };
+	int ret;
+
+	sock  = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock < 0)
+		return -1;
+
+	strncpy(ifr.ifr_name, iface, IFNAMSIZ - 1);
+
+	ret = ioctl(sock, SIOCGIFFLAGS, &ifr);
+	if (ret < 0)
+		return -1;
+
+	ifr.ifr_flags |= IFF_UP | IFF_RUNNING;
+
+	ret = ioctl(sock, SIOCSIFFLAGS, &ifr);
+	if (ret < 0)
+		return -1;
+
+	return 0;
+}
 
 static int get_mac(const char *iface, uint8_t *mac)
 {
@@ -210,22 +258,127 @@ static int get_mac(const char *iface, uint8_t *mac)
 	return 0;
 }
 
+static bool check_packet(struct context *cntx, struct dhcp_packet *p, unsigned int op)
+{
+	if (p->xid != htonl(cntx->xid)) {
+		verbose("XID doesn't match\n");
+		return false;
+	}
+
+	if (p->op != op) {
+		verbose("op is wrong\n");
+		return false;
+	}
+
+
+	return true;
+}
+
+static int find_opt(struct dhcp_packet *p, uint8_t code, uint8_t **opt, unsigned int *len)
+{
+	uint8_t *_opt = p->options;
+	uint8_t *opt_end = p->options + sizeof(p->options);
+
+	while (_opt < opt_end) {
+		uint8_t _code = *_opt++;
+		uint8_t _len;
+
+		switch(_code) {
+		case OPT_PAD:
+			break;
+		case OPT_END:
+			goto out;
+		default:
+			_len = *_opt++;
+
+			if (_code == code) {
+				*opt = _opt;
+				*len = _len;
+				return 0;
+			}
+			else
+				_opt += _len;
+			break;
+		}
+    }
+
+out:
+    return -ENOENT;
+}
+
+static int find_opt_u8(struct dhcp_packet *p, uint8_t code, uint8_t *opt)
+{
+	unsigned int len;
+	uint8_t *_opt;
+	int ret;
+
+	ret = find_opt(p, code, &_opt, &len);
+	if (ret)
+		return ret;
+
+	if (len != 1)
+		return -EINVAL;
+
+	*opt = _opt[0];
+
+	return 0;
+}
+
 int main(int argc, char **argv, char **envp)
 {
 	struct context cntx = {
 		.interface = DEFAULT_INTERFACE,
 	};
+	struct dhcp_packet p;
 	int ret, sock;
 
+	verbose("Bringing %s up\n", cntx.interface);
+	ret = ifup(cntx.interface);
+	if (ret)
+		return 1;
+
+	verbose("Getting MAC address for %s\n", cntx.interface);
 	ret = get_mac(cntx.interface, cntx.mac);
 	if (ret)
 		return 1;
 
+	verbose("Creating broadcast socket\n");
 	ret = setup_socket(&cntx);
 	if (ret)
 		return 1;
 
-	ret = send_discover(&cntx);
+	while (true) {
+		verbose("Sending broadcast\n");
+		ret = send_discover(&cntx, &p);
+
+		verbose("Waiting for response\n");
+		ret = wait_for_packet(&cntx, &p);
+
+		if (ret) {
+			verbose("Didn't get response\n");
+			sleep(10);
+			continue;
+		}
+
+		if (!check_packet(&cntx, &p, 2)) {
+			verbose("incorrect packet?\n");
+			sleep(10);
+			continue;
+		}
+
+		uint8_t msgtype;
+		ret = find_opt_u8(&p, OPT_MSG_TYPE, &msgtype);
+		if (ret) {
+			verbose("Failed to get msgtype\n");
+			sleep(10);
+			continue;
+		}
+
+		if (msgtype == DHCPOFFER) {
+			verbose("got offer\n");
+			break;
+		}
+	}
 
 	return 0;
 }
