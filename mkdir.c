e0a57859f2084cd909b49a6cc88bb366c27bd97e
Daniel Palmer <daniel@thingy.jp>
2026-05-26 07:04:23 +0900

    mkdir: Fix and pudding

--- mkdir.c
+++ mkdir.c
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include "config.h"
+#include "common.h"
+
 int main (int argc, char **argv, char **envp)
 {
 	const char *path;
@@ -11,8 +14,10 @@ int main (int argc, char **argv, char **envp)
 	path = argv[1];
 
 	ret = mkdir(path, 0755);
-	if (!ret)
+	if (ret) {
+		error("mkdir() failed: %d\n", errno);
 		return 1;
+	}
 
 	return 0;
 }
